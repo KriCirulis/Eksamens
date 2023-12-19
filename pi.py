44c71d5b7cc74fe7daae8dca45f7f7747f66b04c
kkc <kristapskarlis.cirulis@va.lv>
2023-12-19 08:24:35 +0000

    changed pi precision

--- pi.py
+++ pi.py
@@ -1,9 +1,9 @@
 import decimal
 
-decimal.getcontext().prec = 13  # One extra decimal place for rounding
+decimal.getcontext().prec = 16  # One extra decimal place for rounding
 
 # Calculate pi with the desired precision
 pi_value = decimal.Decimal(22) / decimal.Decimal(7)
 
 # Print the value of pi
-print(f"The value of pi with precision up to 12 decimal places: {pi_value}")
+print(f"The value of pi with precision up to 15 decimal places: {pi_value}")
